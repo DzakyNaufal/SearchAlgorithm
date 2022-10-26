7469675221da2b920f6663dc54f204dad83e1c6e
DzakyNaufal <dzakynmulna@gmail.com>
2022-10-26 16:16:15 +0700

    menambahkan text

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -25,7 +25,18 @@ namespace SearchAlgorithm
                 if ((n > 0) && (n <= 20))
                     break;
                 else
-                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements");
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements. \n");
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(" Enter array elements");
+            Console.WriteLine("---------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<"+(i+1)+">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
             }
         }
 
